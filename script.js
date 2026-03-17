66c07ede5be3c37fb964b24f61c313027aa4151c
NicholasLupo <nicholaslupo14@gmail.com>
2026-03-17 14:10:16 +0100

    create function to turn on the light

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const lampOn = document.getElementById('lampOn');
 
 console.log(lampOff);
 console.log(lampOn);
+
+function getLight () {
+
+    lampOff.classList.remove('d-block')
+    lampOff.classList.add('d-none');
+
+    lampOn.classList.remove('d-none');
+    lampOn.classList.add('d-block');
+
+}
