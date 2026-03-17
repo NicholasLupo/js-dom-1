44a5767a02b4d33ea9a08d3655ffe97736b37900
NicholasLupo <nicholaslupo14@gmail.com>
2026-03-17 19:30:37 +0100

    add switch on/off

--- script.js
+++ script.js
@@ -4,18 +4,38 @@ const lampOn = document.getElementById('lampOn');
 console.log(lampOff);
 console.log(lampOn);
 
-const btn = document.querySelector('button');
+const btn = document.querySelector('input');
 console.log(btn);
 
+let status = btn.value = 'Accendi';
+let statusOn = status.includes('Accendi');
 
-btn.addEventListener('click', function getLight () {
+btn.addEventListener('click', function getLight() {
 
-    lampOff.classList.remove('d-block')
-    lampOff.classList.add('d-none');
+    if (statusOn == true) {
 
-    lampOn.classList.remove('d-none');
-    lampOn.classList.add('d-block');
+        lampOff.classList.remove('d-block')
+        lampOff.classList.add('d-none');
 
-    btn.innerText = 'Spegni';
+        lampOn.classList.remove('d-none');
+        lampOn.classList.add('d-block');
 
+        btn.value = 'Spegni';
+        statusOn = !statusOn;
+        console.log(statusOn);
+
+
+    } else {
+
+        lampOff.classList.add('d-block')
+        lampOff.classList.remove('d-none');
+
+        lampOn.classList.add('d-none');
+        lampOn.classList.remove('d-block');
+
+        btn.value = 'Accendi';
+        statusOn = !statusOn
+        console.log(statusOn);
+
+    }
 })
